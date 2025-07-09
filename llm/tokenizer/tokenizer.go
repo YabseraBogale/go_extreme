90dbfb40b5eef6251f09a401cc672b277422e8af
YabseraBogale <yabserapython@gmail.com>
2025-07-09 19:44:24 +0300

    -1 for nil and thinking what to make for 'endoftext'

--- llm/tokenizer/tokenizer.go
+++ llm/tokenizer/tokenizer.go
@@ -43,7 +43,7 @@ func (t *Tokenizer) Decode(ids []int) string {
 		if k == -1 {
 			keys = append(keys, t.IntToString[k])
 		} else {
-			keys = append(keys, "nil")
+			keys = append(keys, "<|unk|>")
 		}
 
 	}

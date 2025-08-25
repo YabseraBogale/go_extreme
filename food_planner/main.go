d6df1937df722fbc0f1f6cb9841236a607660638
YabseraBogale <yabserapython@gmail.com>
2025-08-25 19:12:31 +0300

    ok

--- food_planner/main.go
+++ food_planner/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp, err := template.ParseFiles("/public/index.html")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = temp.Execute(w, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+	})
+
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }

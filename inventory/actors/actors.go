2cd5700503134e58d50bbcebcef6ae19463ce51f
YabseraBogale <yabserapython@gmail.com>
2025-09-11 12:54:06 +0300

    writting the database

---
+++ inventory/actors/actors.go
@@ -0,0 +1,16 @@
+package actors
+
+type Employee struct {
+	FirstName   string
+	MiddleName  string
+	LastName    string
+	PhoneNumber string
+	Email       string
+}
+
+type Item struct {
+	ItemName        string
+	ItemDescription string
+	ItemPrice       string
+	ItemQuantity    string
+}

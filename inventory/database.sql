f43853bbc99cc3e5b24267a6906af85c3421e83f
YabseraBogale <yabserapython@gmail.com>
2025-09-12 20:00:17 +0300

    writting the database

--- inventory/database.sql
+++ inventory/database.sql
@@ -7,10 +7,24 @@ create table Employee(
     email text not null
 )
 
+CREATE TABLE categories (
+    category_id  INT PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE warehouses (
+    warehouse_id   INT PRIMARY KEY AUTO_INCREMENT,
+    name           VARCHAR(30) NOT NULL,
+    location       VARCHAR(30)
+);
+
 create table Item(
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     item_name text not null,
     item_decrption text not null,
     item_price float not null,
     item_quantity int not null,
+    category_id references categories,
+    warehouse_id references warehouses
+
 )

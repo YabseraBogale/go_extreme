e4864103c71e29ae211fbf484706508c498cc03f
YabseraBogale <yabserapython@gmail.com>
2025-09-13 19:21:31 +0300

    writting the database

--- inventory/actors/actors.go
+++ inventory/actors/actors.go
@@ -1,6 +1,9 @@
 package actors
 
+import "time"
+
 type Employee struct {
+	EmployeeId  int
 	FirstName   string
 	MiddleName  string
 	LastName    string
@@ -9,8 +12,35 @@ type Employee struct {
 }
 
 type Item struct {
+	EmployeeId      int
+	ItemId          int
 	ItemName        string
 	ItemDescription string
 	ItemPrice       string
 	ItemQuantity    string
+	Categories      Categories
+	Warehouse       Warehouses
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type Warehouses struct {
+	WarehouseId int
+	Name        string
+	Location    string
+}
+
+type Categories struct {
+	CategoryId int
+	Name       string
+}
+
+type StockMovements struct {
+	EmployeeId   int
+	MovementId   int
+	ItemId       Item
+	WarehouseId  Warehouses
+	ChangeQty    int
+	MovementType string
+	CreatedAt    time.Time
 }

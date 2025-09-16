3ac4f7ec88a2401176638200ce184335f2530cdb
YabseraBogale <yabserapython@gmail.com>
2025-09-16 20:40:25 +0300

    fixing nearly done

--- inventory/actors/actors.go
+++ inventory/actors/actors.go
@@ -23,24 +23,3 @@ type Item struct {
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 }
-
-type Warehouses struct {
-	WarehouseId int
-	Name        string
-	Location    string
-}
-
-type Categories struct {
-	CategoryId int
-	Name       string
-}
-
-type StockMovements struct {
-	EmployeeId   int
-	MovementId   int
-	ItemId       Item
-	WarehouseId  Warehouses
-	ChangeQty    int
-	MovementType string
-	CreatedAt    time.Time
-}
